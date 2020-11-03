8690d7bfa70ad865dac2928c0cfe0587e46d4e40
abhi195 <abhisapariya@gmail.com>
2020-11-03 19:22:45 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taxi Fleet Management
+
+REST APIs to manage driver and car fleet for a ride hailing service.
---
+++ src/main/java/com/triptaxi/TaxiFleetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.triptaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiFleetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiFleetManagementApplication.class, args);
+	}
+
+}
